c4503633e7f1ea1ad287a853784ddaff9915d63b
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-05-21 18:08:50 -0500

    fix: :bug: Fix navbar with Javascript
    Fix event listener of navbar to mobile screen

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,12 @@
-const menuButton =  document.querySelector(".hamburger-button");
+const menuButton = document.querySelector(".hamburger-button");
 const menuOptions = document.querySelectorAll(".menu-item");
 const iconMenu = document.querySelector(".fas.fa-bars");
-const logo = document.querySelector('.logo');
-menuButton.addEventListener('click', function(event) {
-    logo.classList.toggle("hidden");
-    iconMenu.classList.toggle("fa-bars");
-    iconMenu.classList.toggle("fa-times");
-    menuOptions.forEach((option) => {
-        option.classList.toggle('hidden');
-    })
+const logo = document.querySelector(".logo");
+menuButton.addEventListener("click", function (event) {
+  logo.classList.toggle("hidden");
+  iconMenu.classList.toggle("fa-bars");
+  iconMenu.classList.toggle("fa-times");
+  menuOptions.forEach((option) => {
+    option.classList.toggle("hidden");
+  });
 });
