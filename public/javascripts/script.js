6155a62dc89f788fe645f0666e95efa60d887e9a
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-05-24 16:42:20 -0500

    add Media Query List API for navbar menu options display for Desktop

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,8 @@ const menuButton = document.querySelector(".hamburger-button");
 const menuOptions = document.querySelectorAll(".menu-item");
 const iconMenu = document.querySelector(".fas.fa-bars");
 const logo = document.querySelector(".logo");
+const mql =  window.matchMedia("(min-width:1025px)");
+
 menuButton.addEventListener("click", function (event) {
   logo.classList.toggle("hidden");
   iconMenu.classList.toggle("fa-bars");
@@ -10,3 +12,20 @@ menuButton.addEventListener("click", function (event) {
     option.classList.toggle("hidden");
   });
 });
+
+
+function displayNavMenuOptions(e) {
+  if (e.matches) {
+    menuOptions.forEach( option => {
+      option.classList.remove("hidden");
+    });
+    menuButton.classList.add("hidden");
+  } else {
+    menuOptions.forEach( option => {
+      option.classList.add("hidden");
+    });
+    menuButton.classList.remove("hidden");
+  }
+}
+
+mql.addEventListener('change', displayNavMenuOptions);
